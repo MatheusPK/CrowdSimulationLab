fc6e938cf4f45f59bf24244488077ca331f88965
Matheus Kulick <matheuskulick@hotmail.com>
2026-03-20 16:12:03 -0300

    add: new maps

--- src/config.py
+++ src/config.py
@@ -8,11 +8,19 @@ maps = [
     "maps/medium/medium_easy.txt",
     "maps/medium/medium_medium.txt",
     "maps/medium/medium_hard.txt",
-    "maps/DI_primeiro_andar.txt"
+    "maps/DI_primeiro_andar.txt",
+    "maps/test_maps/mall_atrium.txt",
+    "maps/test_maps/mall_corridor.txt",
+    "maps/test_maps/mall_emergency.txt",
+    "maps/test_maps/office_complex.txt",
+    "maps/test_maps/office_hazard.txt",
+    "maps/test_maps/office_junction.txt",
+    "maps/test_maps/office_openplan.txt",
+    "maps/test_maps/office_simples.txt",
 ]
 
 CONFIG = ScenarioConfigFactory.build(
-    scenario=AppScenario.DQN_FSM,
-    map_path=maps[0],
+    scenario=AppScenario.ASTAR_FSM,
+    map_path=maps[7],
     agents=10
 )
